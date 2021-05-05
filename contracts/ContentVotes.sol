52e6703192ef44b8166fcd7c36b22d01e591b142
Samuel Peters <peterssam@me.com>
2021-05-05 16:44:46 -0700

    Removing content ranking

--- contracts/ContentVotes.sol
+++ contracts/ContentVotes.sol
@@ -16,36 +16,10 @@ struct Content {
 contract ContentVotes {
     mapping (string => Content) votes;
     IVotesAccountant public voteAccountant;
-    Content[100] public topContent;
-    EnumberableSet public allContentIds;
     uint private nextContentIndex = 0;
 
     constructor(address _voteAccountant){
         voteAccountant = IVotesAccountant(_voteAccountant);
-    }
-
-    // function insertTopContent(Content storage _content) private {
-        
-    // }
-    function getTopN(uint memory _n) external view {
-        //sort the set
-        Content[] memory sortedContent = new Content[_n];
-
-        initializes
-        for (uint i = 0; i < allContentIds.length(); i+=1) {  //for loop example
-            // uint j =0;
-            // while (j<_n && votes[allContentIds.at(j)].totalVotes < sortedContent[j].totalVotes){
-            //     j
-            // }
-            sortedContent[i] = votes[allContentIds.at(i)];      
-        }
-
-        
-
-        // for each item in set, insert content
-
-        return sortedContent[0:_n];
-
     }
 
     function vote(string calldata _contentId, uint _amount, bool _withCaption, uint _captionIndex) external {
